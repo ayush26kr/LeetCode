aff113a4d6fd80f4c64354f804102d20b520865e
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-16 11:43:00 +0530

    Create Binary tree preOrder traversal.cpp

---
+++ Tree/Binary tree preOrder traversal.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        stack<TreeNode*>st;
+        vector<int>ans;
+        if(root==NULL)
+            return ans;
+         st.push(root);
+        while(!st.empty())
+        {
+            auto a=st.top();
+            st.pop();
+            ans.push_back(a->val);
+            if(a->right)
+                st.push(a->right);
+            if(a->left)
+                st.push(a->left);
+            
+        }
+        return ans;
+        
+    }
+};
