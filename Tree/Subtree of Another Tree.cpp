c4ca4a30891d91a6e4d420f4a2edbf499ea8fa2d
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2021-09-19 11:01:48 +0530

    Create Subtree of Another Tree.cpp

---
+++ Tree/Subtree of Another Tree.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isIdentical(TreeNode* root, TreeNode* subRoot){
+        if(root==NULL &&subRoot==NULL)return true;
+        if(root==NULL || subRoot==NULL)return false;
+        return(root->val==subRoot->val &&isIdentical(root->left,subRoot->left) &&isIdentical(root->right,subRoot->right));
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(subRoot==NULL)return true;
+        if(root==NULL)return false;
+        if(isIdentical(root,subRoot))return true;
+        return isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
+        
+    }
+};
