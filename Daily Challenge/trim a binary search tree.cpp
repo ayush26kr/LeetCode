e3fc24543e9213519394cbe2d679b4dc63da3ddf
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2022-04-15 07:51:59 +0530

    Create trim a binary search tree.cpp

---
+++ Daily Challenge/trim a binary search tree.cpp
@@ -0,0 +1,13 @@
+if(!root) return nullptr;
+        
+        if(root->val < low) {
+            return trimBST(root->right, low, high);
+        } else if (root->val > high) {
+            return trimBST(root->left, low, high);
+        }
+        
+        root->left = trimBST(root->left, low, high);
+        root->right = trimBST(root->right, low, high);
+        
+        return root;
+    }
