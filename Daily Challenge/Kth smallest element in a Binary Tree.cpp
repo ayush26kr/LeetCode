6b3398deade7ac4f202d317447dc0cd10dba2f79
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2022-04-18 09:37:36 +0530

    Create Kth smallest element in a Binary Tree.cpp

---
+++ Daily Challenge/Kth smallest element in a Binary Tree.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void helper(TreeNode*root,vector<int >&v){
+        if(root==nullptr)return ;
+        helper(root->left,v);
+        v.push_back(root->val);
+        helper(root->right,v);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int>v;
+        helper(root,v);
+        return v[k-1];
+    }
+};
