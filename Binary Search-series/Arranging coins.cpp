fe5bd0c2f90b595a53f3c32f6492774308bbdfe8
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2022-07-30 13:05:26 +0530

    Create Arranging coins.cpp

---
+++ Binary Search-series/Arranging coins.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int num = n, count = 0, i = 1;
+        do{
+            num -= i;
+            count++;
+        }while(num > i++ );
+        return count;
+    }
+};
