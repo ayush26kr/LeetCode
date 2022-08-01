73986a5df6b94739b4f00d562832c423810d878f
AYUSH KUMAR <57504080+ayush26kr@users.noreply.github.com>
2022-08-02 01:15:20 +0530

    Create Sum of square numbers.cpp

---
+++ Binary Search-series/Sum of square numbers.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long int start=0;
+        long long int end=sqrt(c);
+        while(start<=end){
+            if(start*start+end*end==c)
+                return true;
+            else if(start*start+end*end<c)
+                start++;
+            else
+                end--;
+        }
+        return false;
+    }
+};
